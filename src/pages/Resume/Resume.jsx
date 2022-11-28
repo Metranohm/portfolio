b665c42aee34abd8935ed96eb9d74e85c6297dd3
Metranohm <andrewwink@gmail.com>
2022-11-28 16:56:47 -0600

    updates

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -5,12 +5,9 @@ function Resume() {
   
   return (
     <>
-      {/* <div className={styles.link}> */}
-        
-      {/* </div> */}
-      <div className={styles.image}>
+      <embed className={styles.image}>
         <img src="https://i.postimg.cc/jjqxd5kS/Andrew-Winkler-Resume-1.png" alt="resume" />
-      </div>
+      </embed>
       <div className={styles.link}>
       <Link to="Resume.pdf" target="_blank" download>Download my resume!</Link>
       </div>
