a0a4ff334a2c6cd29b95b782d248563f0d415611
Metranohm <andrewwink@gmail.com>
2023-01-27 18:11:43 -0600

    update about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,8 +8,8 @@ function About() {
       
       <img src={Headshot} alt="pic" className={styles.pic}/>
       <p className={styles.radius}>Hi I'm Andrew, I am a software engineer and musician drawn to innovations that ignite curiosity and creative solutions to bring order to the chaos. I seek to bridge the gap in the relationships between people, technology, and products by emphasizing creativity, empathy, and structural expertise.
-      
-      My experience in Human Resources and music instruction make communication & leadership come naturally to me and I thrive in a group work environment.  I look forward to combining these talents with my new skills as a software engineer to create innovative solutions to real-world problems.
+      <br></br>
+      My experience in Human Resources and music instruction make communication & leadership come naturally to me and I thrive in a group work environment.  I look forward to combining these talents with my new skills as a software engineer to design creative solutions to real-world problems.
       </p>
     </div>
     </>
